862af24a098ea7123a480a5f18f5eb7636978cb0
Nussari <viktorandri17@gmail.com>
2025-04-04 01:02:47 +0000

    Grunnur á upphafssíðu

--- src/main/java/vv/intertrain/vidmot/VelkominnController.java
+++ src/main/java/vv/intertrain/vidmot/VelkominnController.java
@@ -1,28 +1,62 @@
 package vv.intertrain.vidmot;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /******************************************************************************
- *  Nafn    : Ebba Þóra Hvannberg
- *  T-póstur: ebba@hi.is
- *  Lýsing  : Controller fyrir Velkominn viðmótið. Getur farið í spurningaviðmótið
- *  eða hætt
+ *  Nafn    : Viktor Andri Hermannsson
+ *  T-póstur: vah32@hi.is
+ *  Lýsing  : Controller fyrir Velkominn viðmótið. Setur inn upplýsingar um
+ *  starf og getur farið í undirbúning, æfingarviðtal, eða hætt.
  *****************************************************************************/
 
 public class VelkominnController {
+    @FXML private TextField nafn;
+    @FXML private TextField starf;
+    @FXML private Button undirbua;
+    @FXML private Button aefa;
+
+    @FXML
+    public void initialize() {
+        undirbua.disableProperty().bind(
+                Bindings.createBooleanBinding(() ->
+                                nafn.getText().trim().isEmpty() ||
+                                        starf.getText().trim().isEmpty(),
+                        nafn.textProperty(),
+                        starf.textProperty()
+                )
+        );
+
+        aefa.disableProperty().bind(
+                Bindings.createBooleanBinding(() ->
+                                nafn.getText().trim().isEmpty() ||
+                                        starf.getText().trim().isEmpty(),
+                        nafn.textProperty(),
+                        starf.textProperty()
+                )
+        );
+    }
 
     /**
      * Skiptir yfir í spurningaviðmótið
-     * @param ignored
      */
-    public void onSpurningar(ActionEvent ignored) {
+    public void onAefa(ActionEvent ignored) {
+        ViewSwitcher.switchTo(View.SPURNINGAR, true);
+    }
+
+    public void onUndirbua(ActionEvent ignored) {
         ViewSwitcher.switchTo(View.SPURNINGAR, true);
     }
 
     /**
      * Skiptir yfir í kveðju viðmótið
-     * @param ignored
      */
     public void onKvedja(ActionEvent ignored) {
         ViewSwitcher.switchTo(View.KVEDJA, true);
     }
+
+
 }
