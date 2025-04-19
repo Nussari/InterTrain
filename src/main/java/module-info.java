447f51c9a03abaa2227311b7fd12afd6f3e83290
Viktor Bjarkason <vbjarkason@gmail.com>
2025-04-19 14:56:44 +0000

    Laga pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module vv.intertrain {
     requires okhttp3;
     requires com.fasterxml.jackson.databind;
     requires com.google.gson;
+    requires spring.web;
+    requires spring.core;
 
 
     opens vv.intertrain.vidmot to javafx.fxml;
